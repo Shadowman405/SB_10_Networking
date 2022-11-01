52046d39c52360dd0d8616ba03e08671134dfee7
m_mitin <maximus405@gmail.com>
2022-11-01 21:59:39 +0300

    Initial Commit

---
+++ SB_10_Networking/MainCollectionViewController.swift
@@ -0,0 +1,115 @@
+//
+//  MainCollectionViewController.swift
+//  SB_10_Networking
+//
+//  Created by Maxim Mitin on 1.11.22.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MainCollectionViewController: UICollectionViewController {
+    
+    private let userAction = UserActions.allCases
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return userAction.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserActionCell", for: indexPath) as! UserActionCell
+        cell.userActionLbl.text = userAction[indexPath.item].rawValue
+    
+        return cell
+    }
+
+}
+
+// MARK: - Extinsions
+
+extension MainCollectionViewController {
+    
+    enum URLExamples: String {
+        case imageURL = "https://m.media-amazon.com/images/M/MV5BNjQ1NDk2Njk4Ml5BMl5BanBnXkFtZTgwNjMzNDk0MDI@._V1_.jpg"
+        case exampleOne = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+        case exampleTwo = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
+        case exampleThree = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+        case exampleFour = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
+    }
+    
+    
+    enum UserActions: String, CaseIterable {
+        case downloadImgae = "Download Image"
+        case exampleOne = "Example One"
+        case exampleTwo = "Example Two"
+        case exampleThree = "Example Three"
+        case exampleFour = "Example Four"
+        case ourCourses = "Our Courses"
+    }
+    
+// MARK: - Alert Controllers
+    
+    private func successAlert() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Success",
+                                          message: "You can see results in debug console",
+                                          preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+    }
+    
+    private func failedAlert() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Failed",
+                                          message: "Error , ,essage recieved to console",
+                                          preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+    }
+    
+ // MARK: - Networking
+    
+    private func exampleOnePressed() {
+        
+    }
+    
+    
+    private func exampleTwoPressed() {
+        
+    }
+    
+    
+    private func exampleThreePressed() {
+        
+    }
+    
+    
+    private func exampleFourPressed() {
+        
+    }
+    
+}
+
+
+extension MainCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 48 , height: 100)
+    }
+}
---
+++ SB_10_Networking/UserActionCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserActionCell.swift
+//  SB_10_Networking
+//
+//  Created by Maxim Mitin on 1.11.22.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userActionLbl: UILabel!
+    
+}
